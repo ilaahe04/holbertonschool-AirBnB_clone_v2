dc4ea8f0adafb2489e70ec1c0beab64fd083f95e
Kananmrzzd <kananmirzazada@gmail.com>
2024-04-02 00:50:12 +0400

    fixed errors

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from os import getenv
-
-STO_TYP = getenv('HBNB_TYPE_STORAGE')
+from models.place import association_table
 
 class Amenity(BaseModel, Base):
-    '''Amenity class'''
-    if STO_TYP == "db":
-        from models.place import association_table
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary=association_table)
-    else:
-        name = ''
+    """ Amenity: Class"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=association_table, viewonly=False)
+
+    def __init__(self, *args, **kwargs):
+        """
+            Init for inherited
+        """
+        super().__init__(*args, **kwargs)
