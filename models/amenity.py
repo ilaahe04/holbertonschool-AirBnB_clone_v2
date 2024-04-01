4f8b435b308c184678399d94ca299f551db6d593
Kananmrzzd <kananmirzazada@gmail.com>
2024-04-02 00:47:38 +0400

    fixed errors

--- models/amenity.py
+++ models/amenity.py
@@ -5,16 +5,16 @@ from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from os import getenv
 
+STO_TYP = getenv('HBNB_TYPE_STORAGE')
 
 class Amenity(BaseModel, Base):
-    """ Amenity: Class"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
-
-    def __init__(self, *args, **kwargs):
-        """
-            Init for inherited
-        """
-        super().__init__(*args, **kwargs)
+    '''Amenity class'''
+    if STO_TYP == "db":
+        from models.place import association_table
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=association_table)
+    else:
+        name = ''
