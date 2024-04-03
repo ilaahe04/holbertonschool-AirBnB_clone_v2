1fb3d596ee34d8968d9e4dbd0e8d6547504adc9f
Kananmrzzd <kananmirzazada@gmail.com>
2024-04-03 19:49:06 +0400

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ Module for testing file storage"""
 import unittest
+import models
 from models.base_model import BaseModel
 from models import storage
-from os import getenv, remove, path
+import os
 
 
-@unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "DBStorage")
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def setUp(self):
         """ Set up test environment """
         del_list = []
@@ -18,58 +19,67 @@ class test_fileStorage(unittest.TestCase):
         for key in del_list:
             del storage._FileStorage__objects[key]
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def tearDown(self):
         """ Remove storage file at end of tests """
         try:
-            remove('file.json')
-        except:
+            os.remove('file.json')
+        except Exception:
             pass
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_obj_list_empty(self):
         """ __objects is initially empty """
         self.assertEqual(len(storage.all()), 0)
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_new(self):
         """ New object is correctly added to __objects """
-        length_before = len(list(storage.all().values()))
         new = BaseModel()
-        new.save()
-        length_after = len(list(storage.all().values()))
-        self.assertTrue(length_after - 1 == length_before)
+        for obj in storage.all().values():
+            temp = obj
+        self.assertTrue(temp is obj)
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_all(self):
         """ __objects is properly returned """
         new = BaseModel()
         temp = storage.all()
         self.assertIsInstance(temp, dict)
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_base_model_instantiation(self):
         """ File is not created on BaseModel save """
         new = BaseModel()
-        self.assertFalse(path.exists('file.json'))
+        self.assertFalse(os.path.exists('file.json'))
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_empty(self):
         """ Data is saved to file """
         new = BaseModel()
         thing = new.to_dict()
         new.save()
         new2 = BaseModel(**thing)
-        self.assertNotEqual(path.getsize('file.json'), 0)
+        self.assertNotEqual(os.path.getsize('file.json'), 0)
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_save(self):
         """ FileStorage save method """
         new = BaseModel()
         storage.save()
-        self.assertTrue(path.exists('file.json'))
+        self.assertTrue(os.path.exists('file.json'))
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_reload(self):
         """ Storage file is successfully loaded to __objects """
         new = BaseModel()
-        new.save()
+        storage.save()
+        storage.reload()
         for obj in storage.all().values():
             loaded = obj
         self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_reload_empty(self):
         """ Load from an empty file """
         with open('file.json', 'w') as f:
@@ -77,33 +87,40 @@ class test_fileStorage(unittest.TestCase):
         with self.assertRaises(ValueError):
             storage.reload()
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_reload_from_nonexistent(self):
         """ Nothing happens if file does not exist """
         self.assertEqual(storage.reload(), None)
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_base_model_save(self):
         """ BaseModel save method calls storage save """
         new = BaseModel()
         new.save()
-        self.assertTrue(path.exists('file.json'))
+        self.assertTrue(os.path.exists('file.json'))
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_type_path(self):
         """ Confirm __file_path is string """
         self.assertEqual(type(storage._FileStorage__file_path), str)
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_type_objects(self):
         """ Confirm __objects is a dict """
         self.assertEqual(type(storage.all()), dict)
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_key_format(self):
         """ Key is properly formatted """
         new = BaseModel()
-        new.save()
         _id = new.to_dict()['id']
-        temp = list(storage.all().keys())[0]
+        for key in storage.all().keys():
+            temp = key
         self.assertEqual(temp, 'BaseModel' + '.' + _id)
 
+    @unittest.skipIf(models.storage_type == 'db', "testing DB storage instead")
     def test_storage_var_created(self):
         """ FileStorage object storage created """
         from models.engine.file_storage import FileStorage
+        print(type(storage))
         self.assertEqual(type(storage), FileStorage)
--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,69 +1,176 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+    Test Case For Place Model and its Test
+"""
+from models.base_model import BaseModel
 from models.place import Place
+import unittest
+import inspect
+import time
+from datetime import datetime
+import pep8 as pcs
+from unittest import mock
+import models
 
 
-class test_Place(test_basemodel):
-    """ """
+class TestPlace(unittest.TestCase):
+    """
+        unitesst for Place class
+    """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Place"
-        self.value = Place
-        self.place1 = Place(city_id="d80e0344-63eb-434a-b1e0-07783522124e",
-                            user_id="d81e0344-63eb-434a-b1e0-07783522124e",
-                            name="Los Pollos Hermanos",
-                            description="Amazing place :)",
-                            number_rooms=3,
-                            number_bathrooms=4,
-                            max_guest=15,
-                            price_by_night=20,
-                            latitude=12.4,
-                            longtitude=14.12,
-                            amenity_ids=[])
+    def issub_class(self):
+        """
+            test if Place class is sub class of base model
+        """
+        place = Place()
+        self.assertIsInstance(place, BaseModel)
+        self.assertTrue(hasattr(place, "id"))
+        self.assertTrue(hasattr(place, "created_at"))
+        self.assertTrue(hasattr(place, "update_at"))
 
-    def test_city_id(self):
-        """ """
-        self.assertEqual(type(self.place1.city_id), str)
+    def test_city_id_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "city_id"))
+        if models.storage_type == "db":
+            self.assertEqual(place.city_id, None)
 
-    def test_user_id(self):
-        """ """
-        self.assertEqual(type(self.place1.user_id), str)
+    def test_user_id_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "user_id"))
+        if models.storage_type == "db":
+            self.assertEqual(place.user_id, None)
+        else:
+            pass
 
-    def test_name(self):
-        """ """
-        self.assertEqual(type(self.place1.name), str)
+    def test_name_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "name"))
+        if models.storage_type == "db":
+            self.assertEqual(place.name, None)
+        else:
+            pass
 
-    def test_description(self):
-        """ """
-        self.assertEqual(type(self.place1.description), str)
+    def test_description_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "description"))
+        if models.storage_type == "db":
+            self.assertEqual(place.description, None)
+        else:
+            pass
 
-    def test_number_rooms(self):
-        """ """
-        self.assertEqual(type(self.place1.number_rooms), int)
+    def test_number_rooms_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "number_rooms"))
+        if models.storage_type == "db":
+            self.assertEqual(place.number_rooms, None)
 
-    def test_number_bathrooms(self):
-        """ """
-        self.assertEqual(type(self.place1.number_bathrooms), int)
+    def test_number_bathrooms_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "number_bathrooms"))
+        if models.storage_type == "db":
+            self.assertEqual(place.number_bathrooms, None)
+        else:
+            pass
 
-    def test_max_guest(self):
-        """ """
-        self.assertEqual(type(self.place1.max_guest), int)
+    def test_max_guest_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "max_guest"))
+        if models.storage_type == "db":
+            self.assertEqual(place.max_guest, None)
+        else:
+            pass
 
-    def test_price_by_night(self):
-        """ """
-        self.assertEqual(type(self.place1.price_by_night), int)
+    def test_price_by_night_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "price_by_night"))
+        if models.storage_type == "db":
+            self.assertEqual(place.price_by_night, None)
+        else:
+            pass
 
-    def test_latitude(self):
-        """ """
-        self.assertEqual(type(self.place1.latitude), float)
+    def test_latitude_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "latitude"))
+        if models.storage_type == "db":
+            self.assertEqual(place.latitude, None)
+        else:
+            pass
 
-    def test_longitude(self):
-        """ """
-        self.assertEqual(type(self.place1.latitude), float)
+    def test_latitude_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "longitude"))
+        if models.storage_type == "db":
+            self.assertEqual(place.longitude, None)
+        else:
+            pass
 
-    def test_amenity_ids(self):
-        """ """
-        self.assertEqual(type(self.place1.amenity_ids), list)
+    def test_amenity_ids_attr(self):
+        """
+            Test Class attribute
+        """
+        place = Place()
+        self.assertTrue(hasattr(place, "amenity_ids"))
+        if models.storage_type == "db":
+            self.assertEqual(len(place.amenity_ids), 0)
+        else:
+            pass
+
+    def test_to_dictPlace(self):
+        """
+            test to dict method with Place and the type
+            and content
+        """
+        place = Place()
+        dict_cont = place.to_dict()
+        self.assertEqual(type(dict_cont), dict)
+        for attr in place.__dict__:
+            self.assertTrue(attr in dict_cont)
+            self.assertTrue("__class__" in dict_cont)
+
+    def test_dict_value(self):
+        """
+            test the returned dictionar values
+        """
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        place = Place()
+        dict_con = place.to_dict()
+        self.assertEqual(dict_con["__class__"], "Place")
+        self.assertEqual(type(dict_con["created_at"]), str)
+        self.assertEqual(type(dict_con["updated_at"]), str)
+        self.assertEqual(
+            dict_con["created_at"],
+            place.created_at.strftime(time_format)
+        )
+        self.assertEqual(
+            dict_con["updated_at"],
+            place.updated_at.strftime(time_format))
