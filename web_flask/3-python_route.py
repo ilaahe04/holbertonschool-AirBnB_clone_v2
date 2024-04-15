f2417c2e97a172c0a8f5a1ffee8dde68ea00e0ed
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-04-15 16:37:38 +0400

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""save this as app.py"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    display_text = text.replace('_', ' ')
+    return 'C ' + display_text
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    display_text = text.replace('_', ' ')
+    return 'Python ' + display_text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
