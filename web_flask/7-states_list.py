7aa6ae544672eb9d6ce1eeff8074f06d7de84e5a
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-04-16 21:04:58 +0400

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+
+from flask import Flask, render_template_string
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = list(storage.all("State").values())
+    states.sort(key=lambda state: state.name)
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
