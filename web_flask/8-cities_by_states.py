07ff0732ffad8d39b966317cba6bef85f55e71d4
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-04-18 18:02:07 +0400

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,7 +2,8 @@
 """Starts a Flask web application"""
 
 from flask import Flask, render_template
-from models import storage, State, City
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
