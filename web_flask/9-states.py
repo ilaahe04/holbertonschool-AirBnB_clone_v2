52080392db23cb8e4bc16dc49b2ca695e133c2a3
root <root@ACC-DT-DE-BM04>
2024-04-18 18:38:51 +0400

    state

---
+++ web_flask/9-states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Script to start a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    """Display a HTML page with list of all State objects"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_cities(id):
+    """Display a HTML page with list of City objects for a given State"""
+    state = storage.get(State, id)
+    if state:
+        cities = sorted(state.cities, key=lambda city: city.name)
+        return render_template('9-states.html', state=state, cities=cities)
+    else:
+        return render_template('9-states.html', state=None, cities=None)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
