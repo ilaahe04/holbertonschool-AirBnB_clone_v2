c2f9e53f515d09ab71d6fcd6aa4faae1ba78e83b
ilaahe04 <ilaha.hybtv04@gmail.com>
2024-04-20 14:52:14 +0400

    states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,48 +1,42 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
 """
-Script that starts a Flask web application
-"""
-from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    Method to handle teardown
-    """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def states_list():
-    """
-    Route to display a HTML page with a list of all State objects
-    """
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def states_cities(id):
-    """
-    Route to display a HTML page with cities of a State
-    """
-    state = storage.get(State, id)
-    if state is None:
-        return render_template('9-states.html', state=None)
-    else:
-        if storage._DBStorage__engine == 'db':
-            cities = sorted(state.cities, key=lambda x: x.name)
-        else:
-            cities = sorted(state.cities(), key=lambda x: x.name)
-        return render_template('9-states.html', state=state, cities=cities)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
